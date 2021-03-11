9a2fe2114f2f445071bdacfe2f63c536ef6532d7
bego99 <begoja@edu.aau.at>
2021-03-11 15:32:04 +0100

    first commit

---
+++ app/src/main/java/com/example/se2_trail/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.se2_trail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    Button sendButton;
+    EditText inputNumber;
+    TextView  txtViewEingabeMatrikelNr, txtViewAntwort;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendButton = (Button)findViewById(R.id.button);
+        inputNumber = (EditText)findViewById(R.id.editTextNumber);
+        txtViewEingabeMatrikelNr = (TextView)findViewById(R.id.textView);
+        txtViewAntwort = (TextView)findViewById((R.id.textView2));
+    }
+}
